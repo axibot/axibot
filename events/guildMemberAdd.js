1bf39b58f19208c138a9e01da74f2a7811268c45
axibot <47079285+axibot@users.noreply.github.com>
2019-01-28 10:31:02 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage(' **' + username + '**  Sunucuya Hoş Geldin :wink: Keyifli Vakit Geçirmen Dileğiyle https://appstickers-cdn.appadvice.com/1179987344/819903460/e2acb9440bac712b13e3d3f9ad20d909-10.gif    Axi Army Official : https://discord.gg/VFKuNVZ ');
+
+};
